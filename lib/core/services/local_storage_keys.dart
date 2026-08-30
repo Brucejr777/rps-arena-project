210dc4e2a754f2194ea3f7e4ba73bdbad54a8e0e
Ryden-123 <rflendon@gmail.com>
2026-08-30 11:29:56 +0800

    T08: local storage keys

---
+++ lib/core/services/local_storage_keys.dart
@@ -0,0 +1,13 @@
+class LocalStorageKeys {
+  LocalStorageKeys._();
+
+  static const String selectedAppColor = 'selectedAppColor';
+  static const String selectedTheme = 'selectedTheme';
+  static const String masterVolume = 'masterVolume';
+  static const String musicVolume = 'musicVolume';
+  static const String soundEffectsVolume = 'soundEffectsVolume';
+  static const String animationSpeed = 'animationSpeed';
+  static const String victoryAnimationsEnabled = 'victoryAnimationsEnabled';
+  static const String vibrationEnabled = 'vibrationEnabled';
+  static const String localStatistics = 'localStatistics';
+}
