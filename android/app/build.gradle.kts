7856385db7535898d05878b654e036b8ee5d4b9d
Ryden-123 <rflendon@gmail.com>
2026-08-30 11:55:52 +0800

    T09/T09A: placeholder hand assets imported and verified

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.rpsarena.rps_arena"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
--- lib/main.dart
+++ lib/main.dart
@@ -115,6 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            
           ],
         ),
       ),
